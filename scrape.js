d0bd34bba9135096a824d118a4f15a447bd14c3f
Roshan Shrestha <roshanshrestha01@gmail.com>
2018-12-09 13:39:00 +0545

    catch err in every event detail fetch

--- scrape.js
+++ scrape.js
@@ -125,182 +125,186 @@ let scrape = async () => {
         event_id = event_ids[idx]
         const event_detail_url = event_url.replace('${eventId}', event_id)
         console.log('Fetching...', event_detail_url)
-        await page.goto(event_detail_url);
-        await page.waitFor(2)
-        const event_data = await page.evaluate(async (event_id) => {
-            let sleep = (time) => {
-                return new Promise((resolve) => setTimeout(resolve, time));
-            }
-
-            let getInnerHTMLOrNull = (element) => {
-                if (element)
-                    return element.innerHTML
-                return null
-            }
-
-            let getAttributeOrNull = (element, attr) => {
-                if (element)
-                    return element.getAttribute(attr)
-                return null
-            }
-
-            await sleep(500)
-
-            let dct = {}
-
-            let selector = {
-                'title': '#seo_h1_tag',
-                'date': '#title_subtitle > span',
-                'venue': {
-                    'one_day': '._xkh a._5xhk',
-                    'multi_day': '._xkh a._5xhk',
-                    'video': '._xkh a'
-                },
-                'location': {
-                    'one_day': '._xkh a._5xhk + div',
-                    'multi_day': '._xkh a._5xhk + div',
-                    'video': '._xkh a + div',
-                },
-                'timeinfo': {
-                    'one_day': '#event_time_info > div > table > tbody > tr > td._51m-._4930._phw._51mw > div > div > div:nth-child(2) > div > div._2ycp._5xhk',
-                    'multi_day': '#event_time_info > a > table > tbody > tr > td._51m-._51mw > div > div._4dpf._phw > div > div:nth-child(2) > div > div._2ycp._5xhk',
-                    'video': '#event_time_info > div > table > tbody > tr > td._51m-._4930._phw._51mw > div > div > div:nth-child(2) > div > div._2ycp._5xhk',
-                },
-                'organizer': {
-                    'one_day': '#title_subtitle > div > div > div > div > div > a:nth-child(1)',
-                    'multi_day': '#title_subtitle > div > a',
-                    'video': '#title_subtitle > div > div > div > div > div > a',
+        try {
+            await page.goto(event_detail_url);
+            await page.waitFor(2)
+            const event_data = await page.evaluate(async (event_id) => {
+                let sleep = (time) => {
+                    return new Promise((resolve) => setTimeout(resolve, time));
                 }
-            }
 
-            const title = document.querySelector(selector.title)
-            const date = document.querySelector(selector.date)
+                let getInnerHTMLOrNull = (element) => {
+                    if (element)
+                        return element.innerHTML
+                    return null
+                }
 
-            let selector_key = 'one_day'
+                let getAttributeOrNull = (element, attr) => {
+                    if (element)
+                        return element.getAttribute(attr)
+                    return null
+                }
 
-            if (!date) {
-                selector_key = 'multi_day'
-            }
+                await sleep(500)
 
-            const video = document.querySelector('video#u_0_13')
-            if (video) {
-                selector_key = 'video'
-            }
+                let dct = {}
 
-            const venue = document.querySelector(selector.venue[selector_key])
-            const location = document.querySelector(selector.location[selector_key])
-            const timeinfo = document.querySelector(selector.timeinfo[selector_key])
-            const organizer = document.querySelector(selector.organizer[selector_key])
-            dct['id'] = event_id
-            dct['selector'] = selector_key
-            dct['title'] = getInnerHTMLOrNull(title)
-            dct['date'] = getAttributeOrNull(date, 'title')
-            dct['venue'] = getInnerHTMLOrNull(venue)
-            dct['location'] = getInnerHTMLOrNull(location)
-            dct['time_info'] = getInnerHTMLOrNull(timeinfo)
-            dct['time_content'] = getAttributeOrNull(timeinfo, 'content')
-            dct['organizer'] = getInnerHTMLOrNull(organizer)
-
-            const see_more = document.querySelector('._63eo')
-            if (see_more)
-                see_more.click()
-
-            const description = document.querySelector('div._63ew')
-            dct['description'] = getInnerHTMLOrNull(description)
-
-            if (selector_key == 'multi_day') {
-                const events_date = document.querySelectorAll('._1oa6 > div')
-                let _event_date = []
-                let _time_content = []
-                for (let i = 0; i < events_date.length; i++) {
-                    const event_date = events_date[i];
-                    event_date.click()
-                    await sleep(500)
-                    const _datetimediv = document.querySelector(selector.timeinfo[selector_key])
-                    const datetime_content = getAttributeOrNull(_datetimediv, 'content')
-                    _time_content.push(datetime_content)
-                    if (datetime_content) {
-                        const _datetime = datetime_content.split(' to ')
-                        const _start_date = new Date(_datetime[0])
-                        const _end_date = new Date(_datetime[1])
-                        _event_date.push(
-                            [_start_date.toLocaleString(), _end_date.toLocaleString()]
-                        )
+                let selector = {
+                    'title': '#seo_h1_tag',
+                    'date': '#title_subtitle > span',
+                    'venue': {
+                        'one_day': '._xkh a._5xhk',
+                        'multi_day': '._xkh a._5xhk',
+                        'video': '._xkh a'
+                    },
+                    'location': {
+                        'one_day': '._xkh a._5xhk + div',
+                        'multi_day': '._xkh a._5xhk + div',
+                        'video': '._xkh a + div',
+                    },
+                    'timeinfo': {
+                        'one_day': '#event_time_info > div > table > tbody > tr > td._51m-._4930._phw._51mw > div > div > div:nth-child(2) > div > div._2ycp._5xhk',
+                        'multi_day': '#event_time_info > a > table > tbody > tr > td._51m-._51mw > div > div._4dpf._phw > div > div:nth-child(2) > div > div._2ycp._5xhk',
+                        'video': '#event_time_info > div > table > tbody > tr > td._51m-._4930._phw._51mw > div > div > div:nth-child(2) > div > div._2ycp._5xhk',
+                    },
+                    'organizer': {
+                        'one_day': '#title_subtitle > div > div > div > div > div > a:nth-child(1)',
+                        'multi_day': '#title_subtitle > div > a',
+                        'video': '#title_subtitle > div > div > div > div > div > a',
                     }
                 }
-                dct['time_content'] = _time_content
-                dct['event_day'] = _event_date
-            } else {
-                if (dct['time_content']) {
-                    const datetime = dct['time_content'].split(' to ')
-                    const start_date = new Date(datetime[0])
-                    const end_date = new Date(datetime[1])
-                    dct['event_day'] = [[start_date.toLocaleString(), end_date.toLocaleString()]]
+
+                const title = document.querySelector(selector.title)
+                const date = document.querySelector(selector.date)
+
+                let selector_key = 'one_day'
+
+                if (!date) {
+                    selector_key = 'multi_day'
                 }
-            }
 
-
-            const show_map = document.querySelector('#u_0_1g a')
-            if (show_map) {
-                show_map.click()
-                await sleep(500)
-            }
-            const map = document.querySelector('._4j7v > img')
-
-            if (map) {
-                const map_src = getAttributeOrNull(map, 'src')
-                const map_regex = 'markers.*&'
-                const found = map_src.match(map_regex)
-                if (found) {
-                    const marker = found[0].replace('markers=', '').replace('&', '').split('%2C')
-                    dct['latitude'] = marker[0]
-                    dct['longitude'] = marker[1]
+                const video = document.querySelector('video#u_0_13')
+                if (video) {
+                    selector_key = 'video'
                 }
-            }
+
+                const venue = document.querySelector(selector.venue[selector_key])
+                const location = document.querySelector(selector.location[selector_key])
+                const timeinfo = document.querySelector(selector.timeinfo[selector_key])
+                const organizer = document.querySelector(selector.organizer[selector_key])
+                dct['id'] = event_id
+                dct['selector'] = selector_key
+                dct['title'] = getInnerHTMLOrNull(title)
+                dct['date'] = getAttributeOrNull(date, 'title')
+                dct['venue'] = getInnerHTMLOrNull(venue)
+                dct['location'] = getInnerHTMLOrNull(location)
+                dct['time_info'] = getInnerHTMLOrNull(timeinfo)
+                dct['time_content'] = getAttributeOrNull(timeinfo, 'content')
+                dct['organizer'] = getInnerHTMLOrNull(organizer)
+
+                const see_more = document.querySelector('._63eo')
+                if (see_more)
+                    see_more.click()
+
+                const description = document.querySelector('div._63ew')
+                dct['description'] = getInnerHTMLOrNull(description)
+
+                if (selector_key == 'multi_day') {
+                    const events_date = document.querySelectorAll('._1oa6 > div')
+                    let _event_date = []
+                    let _time_content = []
+                    for (let i = 0; i < events_date.length; i++) {
+                        const event_date = events_date[i];
+                        event_date.click()
+                        await sleep(500)
+                        const _datetimediv = document.querySelector(selector.timeinfo[selector_key])
+                        const datetime_content = getAttributeOrNull(_datetimediv, 'content')
+                        _time_content.push(datetime_content)
+                        if (datetime_content) {
+                            const _datetime = datetime_content.split(' to ')
+                            const _start_date = new Date(_datetime[0])
+                            const _end_date = new Date(_datetime[1])
+                            _event_date.push(
+                                [_start_date.toLocaleString(), _end_date.toLocaleString()]
+                            )
+                        }
+                    }
+                    dct['time_content'] = _time_content
+                    dct['event_day'] = _event_date
+                } else {
+                    if (dct['time_content']) {
+                        const datetime = dct['time_content'].split(' to ')
+                        const start_date = new Date(datetime[0])
+                        const end_date = new Date(datetime[1])
+                        dct['event_day'] = [[start_date.toLocaleString(), end_date.toLocaleString()]]
+                    }
+                }
 
 
-            const cover_link = document.querySelector('#event_header_primary > div:nth-child(1) > div._3kwh > a')
-            if (cover_link) {
-                cover_link.click()
-                await sleep(1000)
-                const image = document.querySelector('#photos_snowlift > div._n9 > div > div.fbPhotoSnowliftContainer.snowliftPayloadRoot.uiContextualLayerParent > div.clearfix.fbPhotoSnowliftPopup > div.stageWrapper.lfloat._ohe > div.stage > div._2-sx > img')
-                dct['image_url'] = getAttributeOrNull(image, 'src')
+                const show_map = document.querySelector('#u_0_1g a')
+                if (show_map) {
+                    show_map.click()
+                    await sleep(500)
+                }
+                const map = document.querySelector('._4j7v > img')
+
+                if (map) {
+                    const map_src = getAttributeOrNull(map, 'src')
+                    const map_regex = 'markers.*&'
+                    const found = map_src.match(map_regex)
+                    if (found) {
+                        const marker = found[0].replace('markers=', '').replace('&', '').split('%2C')
+                        dct['latitude'] = marker[0]
+                        dct['longitude'] = marker[1]
+                    }
+                }
+
+
+                const cover_link = document.querySelector('#event_header_primary > div:nth-child(1) > div._3kwh > a')
+                if (cover_link) {
+                    cover_link.click()
+                    await sleep(1000)
+                    const image = document.querySelector('#photos_snowlift > div._n9 > div > div.fbPhotoSnowliftContainer.snowliftPayloadRoot.uiContextualLayerParent > div.clearfix.fbPhotoSnowliftPopup > div.stageWrapper.lfloat._ohe > div.stage > div._2-sx > img')
+                    dct['image_url'] = getAttributeOrNull(image, 'src')
+                } else {
+                    dct['image_url'] = null
+                }
+
+
+                const categories = document.querySelectorAll('div._62hs._4-u3 > div > ul > li > a')
+                let categories_data = []
+                for (let i = 0; i < categories.length; i++) {
+                    const category = categories[i];
+                    categories_data.push(getInnerHTMLOrNull(category))
+                }
+                dct['categories'] = categories_data
+                return dct
+            }, event_id)
+            results.push(event_data)
+            if (event_data.image_url) {
+                try {
+                    await download(event_data.image_url, event_id, function () {
+                        console.log('done')
+                    })
+                } catch (e) {
+                    console.log(e)
+                }
             } else {
-                dct['image_url'] = null
+                image_not_found_events.push(event_id)
+                console.log(`Cannot find image for ${event_id}.`)
             }
 
+            const data = {
+                'events_without_image': image_not_found_events,
+                'results': results
+            }
 
-            const categories = document.querySelectorAll('div._62hs._4-u3 > div > ul > li > a')
-            let categories_data = []
-            for (let i = 0; i < categories.length; i++) {
-                const category = categories[i];
-                categories_data.push(getInnerHTMLOrNull(category))
-            }
-            dct['categories'] = categories_data
-            return dct
-        }, event_id)
-        results.push(event_data)
-        if (event_data.image_url) {
-            try {
-                await download(event_data.image_url, event_id, function () {
-                    console.log('done')
-                })
-            } catch (e) {
-                console.log(e)
-            }
-        } else {
-            image_not_found_events.push(event_id)
-            console.log(`Cannot find image for ${event_id}.`)
+            fs.writeFile(`${data_path}/scrape.json`, JSON.stringify(data), function (err) {
+                console.log("File saved successfully!");
+            });
+        } catch (err) {
+            console.log('Error', err)
         }
-
-        const data = {
-            'events_without_image': image_not_found_events,
-            'results': results
-        }
-    
-        fs.writeFile(`${data_path}/scrape.json`, JSON.stringify(data), function (err) {
-            console.log("File saved successfully!");
-        });
     }
 
     browser.close()
